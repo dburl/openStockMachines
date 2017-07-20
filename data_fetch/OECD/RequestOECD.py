ff6c123275eb501fe43bde18826c0f7323e24284
dburlyav <burlyaev.dmitry@gmail.com>
2017-07-20 03:07:13 +0200

    Minor code fixes

--- data_fetch/OECD/RequestOECD.py
+++ data_fetch/OECD/RequestOECD.py
@@ -1,22 +1,25 @@
-import json, codecs, datetime, math
+import json, codecs
+from datetime import datetime
 from urllib.request import urlopen
 import functools as ft
 
 class RequestInfo:
     """"Defines what Data to be requested"""
     def __init__(self):
-        self.start_date = datetime.strprtime('Jan 1 2000', '%b %d %Y')
-        self.end_date = datetime.strprtime('Jan 1 2017', '%b %d %Y')
+        self.start_date = datetime.strptime('Jan 1 2000', '%b %d %Y')
+        self.end_date = datetime.today()
         self.params = ["startTime=2009-Q2", "endTime=2011-Q4", "detail=serieskeysonly"]
         self.agency = "all"
         self.data_id = "QNA"
         self.filters = ["AUS+AUT", "GDP+B1_GE", "CUR+VOBARSA", "Q"]
 
-    def __date2quarter(self, dt):
+    @staticmethod
+    def __date2quarter(dt):
         return dt.month//4 + 1
 
-    def __date2str(self, dt):
-        return dt.year + '-Q' + self.__date2quarter(self, dt)
+    @staticmethod
+    def __date2str(dt):
+        return str(dt.year) + '-Q' + str(RequestInfo.__date2quarter(dt))
 
     def start_time(self, dt):
         self.start_date = datetime.strprtime(dt, '%b %d %Y')
@@ -29,8 +32,8 @@ class RequestInfo:
 
     def get_descriptor(self):
         filters_str = ft.reduce(lambda accum, el: accum+"."+el, self.filters)
-        self.params[0] ='startTime='+self.date2str(self, self.start_date)
-        self.params[1] = 'endTime=' + self.date2str(self, self.end_date)
+        self.params[0] = 'startTime='+self.__date2str(self.start_date)
+        self.params[1] = 'endTime=' + self.__date2str(self.end_date)
         params_str = ft.reduce(lambda accum, el: accum + "&" + el, self.params)
         return ft.reduce(lambda accum, el: accum + "/" + el, [self.data_id, filters_str, self.agency+"?"+params_str])
 
@@ -56,4 +59,4 @@ if __name__ == "__main__":
     print("OECD API test")
     info = RequestInfo()
     request = Request(info)
-    request.get_data()
+    data = request.get_data()
